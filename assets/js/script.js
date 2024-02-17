d4b4f87ce08d1538123fa123a6b71ce502f0491e
Laxmiray-1011 <laxmiray1011@gmail.com>
2024-02-17 20:57:28 +0530

    header part done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,145 @@
-'use strict';
+// 'use strict';
 
 
 
-/**
- * navbar toggle
- */
+// /**
+//  * navbar toggle
+//  */
 
-const overlay = document.querySelector("[data-overlay]");
-const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-const navbar = document.querySelector("[data-navbar]");
-const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+// const overlay = document.querySelector("[data-overlay]");
+// const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+// const navbar = document.querySelector("[data-navbar]");
+// const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 
-const navElems = [overlay, navOpenBtn, navCloseBtn];
+// const navElems = [overlay, navOpenBtn, navCloseBtn];
 
-for (let i = 0; i < navElems.length; i++) {
-  navElems[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
-  });
-}
+// for (let i = 0; i < navElems.length; i++) {
+//   navElems[i].addEventListener("click", function () {
+//     navbar.classList.toggle("active");
+//     overlay.classList.toggle("active");
+//   });
+// }
 
 
 
-/**
- * header & go top btn active on page scroll
- */
+// /**
+//  * header & go top btn active on page scroll
+//  */
 
-const header = document.querySelector("[data-header]");
-const goTopBtn = document.querySelector("[data-go-top]");
+// const header = document.querySelector("[data-header]");
+// const goTopBtn = document.querySelector("[data-go-top]");
 
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 80) {
-    header.classList.add("active");
-    goTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    goTopBtn.classList.remove("active");
-  }
-});
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY >= 80) {
+//     header.classList.add("active");
+//     goTopBtn.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//     goTopBtn.classList.remove("active");
+//   }
+// });
+(function($){
+  	/*----------------------------------------
+	   Sticky Menu Activation
+	---------------------------------*/
+	$(window).on('scroll', function () {
+		if ($(this).scrollTop() > 300) {
+			$('.header-sticky').addClass('sticky');
+		} else {
+			$('.header-sticky').removeClass('sticky');
+		}
+	});
+	/*----------------------------------------
+		Off Canvas
+	-------------------------------------------*/
+	$(".off-canvas-btn").on('click', function () {
+		$("body").addClass('fix');
+		$(".off-canvas-wrapper").addClass('open');
+	});
+
+	$(".btn-close-off-canvas,.off-canvas-overlay").on('click', function () {
+		$("body").removeClass('fix');
+		$(".off-canvas-wrapper").removeClass('open');
+	});
+
+	/*----------------------------------------
+		Off Canvas Menu
+	-------------------------------------------*/
+	$(".off-canvas-menu-btn").on('click', function () {
+		$("body").addClass('fix');
+		$(".off-canvas-menu-wrapper").addClass('open');
+	});
+
+	$(".btn-close-off-canvas,.off-canvas-overlay").on('click', function () {
+		$("body").removeClass('fix');
+		$(".off-canvas-menu-wrapper").removeClass('open');
+	});
+	/*----------------------------------------*/
+	/*  Cart Plus Minus Button
+	/*----------------------------------------*/
+	$('.cart-plus-minus').append(
+		'<div class="dec qtybutton"><i class="fa fa-minus"></i></div><div class="inc qtybutton"><i class="fa fa-plus"></i></div>'
+	);
+	$('.qtybutton').on('click', function () {
+		var $button = $(this);
+		var oldValue = $button.parent().find('input').val();
+		if ($button.hasClass('inc')) {
+			var newVal = parseFloat(oldValue) + 1;
+		} else {
+			// Don't allow decrementing below zero
+			if (oldValue > 1) {
+				var newVal = parseFloat(oldValue) - 1;
+			} else {
+				newVal = 1;
+			}
+		}
+		$button.parent().find('input').val(newVal);
+	});
+	/*----------------------------------------*/
+	/* Toggle Function Active
+	/*----------------------------------------*/
+	// showlogin toggle
+	$('#showlogin').on('click', function () {
+		$('#checkout-login').slideToggle(900);
+	});
+	// showlogin toggle
+	$('#showcoupon').on('click', function () {
+		$('#checkout_coupon').slideToggle(900);
+	});
+	// showlogin toggle
+	$('#cbox').on('click', function () {
+		$('#cbox-info').slideToggle(900);
+	});
+
+	// Ship box toggle
+	$('#ship-box').on('click', function () {
+		$('#ship-box-info').slideToggle(1000);
+	});
+	/*----------------------------------------
+		Responsive Mobile Menu
+	------------------------------------------*/
+	//Variables
+	var $offCanvasNav = $('.mobile-menu'),
+	$offCanvasNavSubMenu = $offCanvasNav.find('.dropdown');
+	//Add Toggle Button With Off Canvas Sub Menu
+	$offCanvasNavSubMenu.parent().prepend('<span class="menu-expand"><i></i></span>');
+	//Close Off Canvas Sub Menu
+	$offCanvasNavSubMenu.slideUp();
+	//Category Sub Menu Toggle
+	$offCanvasNav.on('click', 'li a, li .menu-expand', function(e) {
+	var $this = $(this);
+	if ( ($this.parent().attr('class').match(/\b(menu-item-has-children|has-children|has-sub-menu)\b/)) && ($this.attr('href') === '#' || $this.hasClass('menu-expand')) ) {
+		e.preventDefault();
+		if ($this.siblings('ul:visible').length){
+			$this.parent('li').removeClass('active');
+			$this.siblings('ul').slideUp();
+		} else {
+			$this.parent('li').addClass('active');
+			$this.closest('li').siblings('li').removeClass('active').find('li').removeClass('active');
+			$this.closest('li').siblings('li').find('ul:visible').slideUp();
+			$this.siblings('ul').slideDown();
+		}
+	}
+	});
+
+})(jQuery);
